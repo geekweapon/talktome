61a571e7a7af148092760cc4884ab625aee710af
nynhex <nynhex@gmail.com>
2017-09-11 02:40:17 -0500

    Strip unnecessary variables

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,6 @@ class MessagesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def reply
-    message_body = params["Body"]
-    from_number = params["From"]
-    SMS.new(from_number, message_body).send
+    SMS.new(params["From"], params["Body"]).send
   end
 end
